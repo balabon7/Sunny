7532d11dd3bf6a3a191c822c84b18f3ca47e3477
Alex Balabon <balabon7@gmail.com>
2020-07-18 12:40:12 +0300

    first commit

---
+++ Sunny/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sunny
+//
+//  Created by mac on 18.07.2020.
+//  Copyright © 2020 Aleksandr Balabon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
